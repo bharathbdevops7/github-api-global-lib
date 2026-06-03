a6a1cddc24486d1132e54a014693c58d5c1796cd
bharathbdevops7 <bharathbdevops@gmail.com>
2026-06-03 01:11:55 -0400

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,7 @@
 
-#def call(Map config = [:]) {
-#    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-#}
+//def call(Map config = [:]) {
+//    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+//}
 
 def call(){
     sh "echo hello world"
