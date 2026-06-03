23e12f31894ff29d1bb7bfd97c48e7425178bae0
bharathbdevops7 <bharathbdevops@gmail.com>
2026-06-03 01:15:20 -0400

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,6 +3,6 @@
 //    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 //}
 
-def call(){
-    sh "echo hello world"
+def call(String name , String dayOfWeek){
+    sh "echo Hello ${name} . Today is ${dayOfWeek}"
 }
